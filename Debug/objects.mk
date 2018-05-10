18978fbb5f03d341e00389070ff863eb9194f984
Lukáš Mandák <Lukas.Mandak@onsemi.com>
2018-05-10 15:45:20 +0200

    Deleted generated binary / build files.

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lblelib -lkelib -lbass -lcmsis -lsyslib -lweak_prf
-
